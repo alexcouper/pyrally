64ef4f1cf6442d613d9e28ee1a20e26854cde99a
Alex Couper <alex.couper@glassesdirect.com>
2011-12-22 17:53:10 +0000

    Bump version, Add authors.

--- setup.py
+++ setup.py
@@ -2,16 +2,32 @@
 # # coding: utf-8
 
 from setuptools import setup
+long_description = open('README').read()
 
 
 setup(
     name='pyrally',
-    description='Client for accessing Rally API.',
-    version='0.1.0',
+    description='Rally API Client',
+    long_description=long_description,
+    version='0.2.0',
     author='Alex Couper',
     author_email='amcouper@gmail.com',
+    url='https://github.com/alexcouper/pyrally',
     packages=['pyrally'],
     install_requires=['simplejson'],
     scripts=[
+    ],
+    package_data={
+        '': ['*.txt', '*.rst'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP',
     ],
 )
