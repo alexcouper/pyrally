e9f4399fee7951d5ab1ff05545d1cb92e6e9a854
Alex Couper <alex.couper@glassesdirect.com>
2011-12-28 15:40:33 +0000

    More readthedocs debugging...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,9 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 docs_root = os.path.dirname(os.path.dirname(__file__))
-print docs_root
+print os.path.split(docs_root)[0]
 sys.path.insert(0, os.path.split(docs_root)[0])
+import pyrally.models
 #sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ----------------------------------------------------
